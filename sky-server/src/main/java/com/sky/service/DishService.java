d046f1a19e62348b3a9e235cb045a31137c0a1cb
15414 <z15971175996@gmail.com>
2026-02-22 22:03:30 +0800

    店铺营业状态设置业务代码开发

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,11 +1,46 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.entity.Dish;
+import com.sky.result.PageResult;
+import com.sky.vo.DishVO;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 public interface DishService {
     /*新增菜品与对应口味数据
     * */
     public void saveWithFlavor(DishDTO dishDTO);
+
+
+    /*菜品分页查询*/
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    /*菜品批量删除*/
+    void deleteBatch(List<Long> ids);
+
+    /*根据id查询菜品和口味*/
+    DishVO getByIdWithFlavor(Long id);
+
+    /*根据id修改菜品和口味*/
+    void updateWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 菜品起售停售
+     * @param status
+     * @param id
+     * @return
+     */
+    void startOrStop(Integer status, Long id);
+
+
+    /**
+     * 根据分类id查询菜品
+     * @param categoryId
+     * @return
+     */
+    List<Dish> list(Long categoryId);
 }
